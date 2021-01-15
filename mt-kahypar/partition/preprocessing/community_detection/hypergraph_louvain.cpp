5059f6af9e6e4bb8689e972fe17f98521b3f3df3
Robert Krause <robert.krause05@web.de>
2021-01-15 18:03:28 +0100

    proper louvain implementation

--- mt-kahypar/partition/preprocessing/community_detection/hypergraph_louvain.cpp
+++ mt-kahypar/partition/preprocessing/community_detection/hypergraph_louvain.cpp
@@ -1,25 +1,28 @@
 #include "mt-kahypar/partition/preprocessing/community_detection/hypergraph_louvain.h"
 
 namespace mt_kahypar::community_detection {
-ds::Clustering hypergraph_local_moving_contract_recurse(ds::CommunityHypergraph& chg, HypergraphLocalMovingModularity& hlmm) {
-    ds::Clustering communities(chg.initialNumNodes());
-    bool clustering_changed = hlmm.localMoving(communities);
+parallel::scalable_vector<HypernodeID> hypergraph_local_moving_contract_recurse(ds::CommunityHypergraph& chg, HypergraphLocalMovingModularity& hlmm) {
+    parallel::scalable_vector<HypernodeID> communities(chg.initialNumNodes());
+    //LOG << "before" << metrics::hyp_modularity(chg, communities, hlmm);
+    bool clustering_changed = hlmm.localMoving(chg, communities);
+    LOG << "after" << metrics::hyp_modularity(chg, communities, hlmm);
+    // if (clustering_changed) {
 
-    if (clustering_changed) {
+    //     ds::StaticHypergraph coars_hg = chg.contract(communities);
+    //     ds::CommunityHypergraph coarse_chg = chg.mapContractedVolumes(coars_hg);
 
-        ds::CommunityHypergraph coars_chg = chg.contract(/*communities*/);
 
-        ds::Clustering coarse_communities = hypergraph_local_moving_contract_recurse(chg, hlmm);
-        for (size_t i = 0; i < chg.initialNumNodes(); ++i) {
-            communities[i] = coarse_communities[communities[i]];
-        }
-    }
+    //     parallel::scalable_vector<HypernodeID> coarse_communities = hypergraph_local_moving_contract_recurse(chg, hlmm);
+    //     for (size_t i = 0; i < chg.initialNumNodes(); ++i) {
+    //         communities[i] = coarse_communities[communities[i]];
+    //     }
+    // }
     return communities;
 }
 
-ds::Clustering hypergraph_louvain(ds::CommunityHypergraph& chg) {
-    HypergraphLocalMovingModularity hlmm(chg);
-    ds::Clustering communities = community_detection::hypergraph_local_moving_contract_recurse(chg, hlmm);
+parallel::scalable_vector<HypernodeID> hypergraph_louvain(ds::CommunityHypergraph& chg, const bool deactivate_random) {
+    HypergraphLocalMovingModularity hlmm(chg, deactivate_random);
+    parallel::scalable_vector<HypernodeID> communities = community_detection::hypergraph_local_moving_contract_recurse(chg, hlmm);
     return communities;
 }
 }
--- mt-kahypar/partition/preprocessing/community_detection/hypergraph_louvain.h
+++ mt-kahypar/partition/preprocessing/community_detection/hypergraph_louvain.h
@@ -1,8 +1,8 @@
-#include "mt-kahypar/partition/preprocessing/community_detection/hypergraph_local_moving_modularity.h";
+#include "mt-kahypar/partition/preprocessing/community_detection/hypergraph_local_moving_modularity.h"
 
 namespace mt_kahypar::community_detection {
 
-ds::Clustering hypergraph_local_moving_contract_recurse(ds::CommunityHypergraph& chg, HypergraphLocalMovingModularity& hlmm);
+parallel::scalable_vector<HypernodeID> hypergraph_local_moving_contract_recurse(ds::CommunityHypergraph& chg, HypergraphLocalMovingModularity& hlmm);
 
-ds::Clustering hypergraph_louvain(ds::CommunityHypergraph& chg);
+parallel::scalable_vector<HypernodeID> hypergraph_louvain(ds::CommunityHypergraph& chg, const bool deactivate_random);
 }
